52664ae7e70b5b0c977891f8f6e00e0894b0c4a5
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-11-27 12:42:00 +0300

    Lesson 7 : I have arranged Con.String to Db and set my DbContext

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SignalR.EntityLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SignalR.DataAccessLayer.Concrete;
-public class SignalRContext
+public class SignalRContext : DbContext
 {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-A6C5CRN\\SQLEXPRESS;initial Catalog=SignalRDb;integrated Security=true");
+    }
+
+    public DbSet<About> Abouts { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Discount> Discounts { get; set; }
+    public DbSet<Feature> Features { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<SocialMedia> SocialMedias { get; set; }
+    public DbSet<Testimonial> Testimonials { get; set; }
 }
