787ee967c9f264c6a39d4cd6f296aeeee4072139
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-11-27 23:37:31 +0300

    Lesson 17-18 : I have made some coding about API section , I tried and saw to work about API

---
+++ SignalRApi/Controllers/AboutController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.AboutDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AboutController : ControllerBase
+{
+    private readonly IAboutService _aboutService;
+
+    public AboutController(IAboutService aboutService)
+    {
+        _aboutService = aboutService;
+    }
+
+    [HttpGet]
+    public IActionResult AboutList()
+    {
+        var values = _aboutService.TGetListAll();
+        return Ok(values);
+    }
+    [HttpPost]
+    public IActionResult CreateAbout(CreateAboutDto createAboutDto)
+    {
+        About about = new About()
+        {
+            Title = createAboutDto.Title,
+            Description = createAboutDto.Description,
+            ImageUrl = createAboutDto.ImageUrl
+        };
+        _aboutService.TAdd(about);
+        return Ok("Section of About is added succesfully");
+    }
+    [HttpDelete]
+    public IActionResult DeleteAbout(int id)
+    {
+        var values = _aboutService.TGetByID(id);
+        _aboutService.TDelete(values);
+        return Ok("Section of About is deleted");  
+    }
+    [HttpPut]
+    public IActionResult UpdateAbout(UpdateAboutDto updateAboutDto)
+    {
+        About about = new About()
+        {
+            AboutID = updateAboutDto.AboutID,
+            Title = updateAboutDto.Title,
+            Description = updateAboutDto.Description,
+            ImageUrl = updateAboutDto.ImageUrl
+        };
+        _aboutService.TUpdate(about);
+        return Ok("Section of About is updated");
+    }
+    [HttpGet("GetAbout")]
+    public IActionResult GetAbout(int id)
+    {
+        var value = _aboutService.TGetByID(id);
+        return Ok(value);
+    }
+}
--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -1,15 +1,24 @@
+using SignalR.BusinessLayer.Abstract;
+using SignalR.BusinessLayer.Concrete;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<SignalRContext>();
+
+builder.Services.AddScoped<IAboutService, AboutManager>();
+builder.Services.AddScoped<IAboutDal, EfAboutDal>();
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
