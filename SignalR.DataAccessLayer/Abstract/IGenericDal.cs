28c022f5bd8c142473b161060992c609eb304954
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-11-27 13:25:22 +0300

    Lesson 9 : DataAccessLayer has been arranged and created EF Dal classes

--- SignalR.DataAccessLayer/Abstract/IGenericDal.cs
+++ SignalR.DataAccessLayer/Abstract/IGenericDal.cs
@@ -1,6 +1,9 @@
-﻿namespace SignalR.DataAccessLayer.Abstract;
+﻿using SignalR.DataAccessLayer.Concrete;
+
+namespace SignalR.DataAccessLayer.Abstract;
 public interface IGenericDal<T> where T : class
 {
+   
     void Add(T entity);
     void Delete(T entity);
     void Update(T entity);
---
+++ SignalR.DataAccessLayer/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.EntityFramework;
+internal class EfCategoryDal : GenericRepository<Category>, ICategoryDal
+{
+    public EfCategoryDal(SignalRContext context) : base(context)
+    {
+    }
+}
