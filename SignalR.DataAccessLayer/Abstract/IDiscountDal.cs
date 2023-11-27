46d8ddb635bb6da630b4d191154a6a78e489fe1c
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-11-27 13:01:13 +0300

    Lesson 8 : I created my Interface in DAL

---
+++ SignalR.DataAccessLayer/Abstract/IDiscountDal.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Abstract;
+public interface IDiscountDal : IGenericDal<Discout>
+{
+}
---
+++ SignalR.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.DataAccessLayer.Abstract;
+public interface IGenericDal<T> where T : class
+{
+    void Add(T entity);
+    void Delete(T entity);
+    void Update(T entity);
+    T GetByID(int id);
+    List<T> GetListAll();   
+}
