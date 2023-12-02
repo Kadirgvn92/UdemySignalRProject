cf1c42882c6c388e904f315f5d0feac259703d47
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-02 16:17:22 +0300

    Lesson 26 : I have configured some productController because of CategorYName was null,

--- SignalRApi/Controllers/ProductController.cs
+++ SignalRApi/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
 using SignalR.DtoLayer.ProductDto;
 using SignalR.EntityLayer.Entities;
 
@@ -25,12 +27,24 @@ public class ProductController : ControllerBase
         var value = _mapper.Map<List<ResultProductDto>>(_productService.TGetListAll());
         return Ok(value);
     }
+
+
     [HttpGet("ProductListWithCategory")]
     public IActionResult ProductListWithCategory()
     {
-        var values = _mapper.Map<List<ResultProductWitCategory>>
-            (_productService.TGetProductsWithCategories());
-        return Ok(values);
+        var context = new SignalRContext();
+        var values = context.Products.Include(x => x.Category).Select(y => new ResultProductWitCategory
+        {
+            Description = y.Description,
+            ImageUrl = y.ImageUrl,
+            Price = y.Price,
+            ProductID = y.ProductID,
+            ProductName = y.ProductName,
+            ProductStatus = y.ProductStatus,
+            CategoryName = y.Category.CategoryName,
+
+        });
+        return Ok(values.ToList());
     }
 
     [HttpPost]
