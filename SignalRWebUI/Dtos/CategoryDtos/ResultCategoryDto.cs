80372c81b48c5fb8d0114c86da4fe7df4ae9da3a
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-04 08:49:53 +0300

    Lesson 32 : Apı and view have been integrated eachother

---
+++ SignalRWebUI/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalRWebUI.Dtos.CategoryDtos;
+
+public class ResultCategoryDto
+{
+	public int CategoryID { get; set; }
+	public string CategoryName { get; set; }
+	public bool Status { get; set; }
+}
