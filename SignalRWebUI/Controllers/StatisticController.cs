db79d03dba9d99aebffb3fe5a5a9c6cdf0f0010d
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-06 15:07:46 +0300

    Lesson 57 : Statistic page has been arranged.

---
+++ SignalRWebUI/Controllers/StatisticController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers;
+public class StatisticController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
