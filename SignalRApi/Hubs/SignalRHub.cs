7b02e23c15d1b6e749ba6827b1cfe8eb7b804f5a
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-07 22:48:25 +0300

    Lesson 87 : ProgressBars are arranged.

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -72,5 +72,17 @@ public class SignalRHub : Hub
 		await Clients.All.SendAsync("ReceiveMenuTableCount", values16);
 	}
 
+	public async Task SendProgress()
+	{
+        var value = _moneyCaseService.TTotalMoneyCaseAmount();
+        await Clients.All.SendAsync("ReceiveTotalMoneyCaseAmount", value.ToString("0.00" + "₺"));
+
+		var value2 = _orderService.TActiveOrderCount();
+        await Clients.All.SendAsync("ReceiveActiveOrderCount", value2);
+
+        var value3 = _menuTableService.TMenuTableCount();
+        await Clients.All.SendAsync("ReceiveMenuTableCount", value3);
+    }
+
 
 }
---
+++ SignalRWebUI/Controllers/ProgressBarsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers;
+public class ProgressBarsController : Controller
+{
+	public IActionResult Index()
+	{
+		return View();
+	}
+}
