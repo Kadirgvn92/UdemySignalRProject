e26ebc8889cdb1d925a5ba55e59788aab14bcb47
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-07 13:43:04 +0300

    Lesson 76-66 : SignalR Statistics arranged

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,15 +1,32 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.BusinessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 
 namespace SignalRApi.Hubs;
 
 public class SignalRHub : Hub
 {
-    SignalRContext context = new SignalRContext();  
+    private readonly ICategoryService _categoryService;
+	private readonly IProductService _productService;
 
-    public async Task SendCategoryCount()
+
+	public SignalRHub(ICategoryService categoryService, IProductService productService)
+	{
+		_categoryService = categoryService;
+		_productService = productService;
+	}
+
+	public async Task SendCategoryCount()
     {
-        var value = context.Categories.Count();
+		var value = _categoryService.TCategoryCount();
         await Clients.All.SendAsync("ReceiveCategoryCount", value);
     }
+
+	public async Task SendProductCount()
+	{
+		var values2 = _productService.TProductCount();
+		await Clients.All.SendAsync("ReceiveProductCount", values2);
+	}
+
+
 }
