10fd14e08dbea647a33f581d5e095fd5e28a9462
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-08 16:47:16 +0300

    Lesson 99 : Testimonial Apı is consumed

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultTestimonialComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultTestimonialComponentPartial.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.TestimonialDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents;
 
 public class _DefaultTestimonialComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _DefaultTestimonialComponentPartial(IHttpClientFactory httpClientFactory)
     {
-        return View();
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7029/api/Testimonial");
+        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+        var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+        return View(values);
     }
 }
