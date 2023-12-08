49277bcbb2a8f2e7574479b423ee1fee86806def
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-08 13:28:48 +0300

    Lesson 94 : Slider table and so API created

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -27,4 +27,5 @@ public class SignalRContext : DbContext
     public DbSet<OrderDetail> OrderDetails { get; set; }
     public DbSet<MoneyCase> MoneyCases { get; set; }
     public DbSet<MenuTable> MenuTables { get; set; }
+    public DbSet<Slider> Sliders { get; set; }
 }
--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -63,6 +63,8 @@ builder.Services.AddScoped<IMoneyCaseDal, EfMoneyCaseDal>();
 builder.Services.AddScoped<IMenuTableService, MenuTableManager>();
 builder.Services.AddScoped<IMenuTableDal, EfMenuTableDal>();
 
+builder.Services.AddScoped<ISliderService, SliderManager>();
+builder.Services.AddScoped<ISliderDal, EfSliderDal>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
