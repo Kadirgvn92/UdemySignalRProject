490b3a3391132b0bcf38ad68148f3ef0cae3b1e4
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-08 17:10:01 +0300

    Lesson 99 : Last Apı's are consumed and completed

--- SignalR.DataAccessLayer/Migrations/20231208135321_mig_add_contact_new_columns.Designer.cs
+++ SignalR.DataAccessLayer/Migrations/20231208135321_mig_add_contact_new_columns.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SignalR.DataAccessLayer.Concrete;
 
@@ -11,9 +12,10 @@ using SignalR.DataAccessLayer.Concrete;
 namespace SignalR.DataAccessLayer.Migrations
 {
     [DbContext(typeof(SignalRContext))]
-    partial class SignalRContextModelSnapshot : ModelSnapshot
+    [Migration("20231208135321_mig_add_contact_new_columns")]
+    partial class mig_add_contact_new_columns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SignalRWebUI/Dtos/ContactDtos/ResultContactDto.cs
+++ SignalRWebUI/Dtos/ContactDtos/ResultContactDto.cs
@@ -5,5 +5,9 @@ public class ResultContactDto
     public string Location { get; set; }
     public string Phone { get; set; }
     public string Mail { get; set; }
+    public string FooterTitle { get; set; }
     public string FooterDescription { get; set; }
+    public string OpenDays { get; set; }
+    public string OpenDaysDescription { get; set; }
+    public string OpenHours { get; set; }
 }
