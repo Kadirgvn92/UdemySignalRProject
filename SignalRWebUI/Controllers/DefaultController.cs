0047df71553f2bd83dbb62401cf8d026eb6599a4
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-08 10:00:41 +0300

    Lesson 88 : I have started UILayout

---
+++ SignalRWebUI/Controllers/DefaultController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers;
+public class DefaultController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutHeadComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.UILayoutComponents;
+
+public class _UILayoutHeadComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
