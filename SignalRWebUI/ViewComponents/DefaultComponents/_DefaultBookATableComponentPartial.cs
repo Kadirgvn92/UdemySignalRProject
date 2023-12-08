f86e135e34017ee35a3ccd831805bc7b0c377403
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-08 13:02:52 +0300

    Lesson 93 : I have completed Default Layout

---
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultBookATableComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.DefaultComponents;
+
+public class _DefaultBookATableComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
