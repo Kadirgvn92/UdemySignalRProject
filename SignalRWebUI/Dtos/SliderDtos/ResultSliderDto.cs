8a34ed8349e256d5d74cb6b6ced93d14cf94ded5
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-08 13:49:56 +0300

    Lesson 95 : Slider API Consumed

---
+++ SignalRWebUI/Dtos/SliderDtos/ResultSliderDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SignalRWebUI.Dtos.SliderDtos;
+
+public class ResultSliderDto
+{
+    public int SliderID { get; set; }
+    public string Title1 { get; set; }
+    public string Title2 { get; set; }
+    public string Title3 { get; set; }
+    public string Description1 { get; set; }
+    public string Description2 { get; set; }
+    public string Description3 { get; set; }
+}
--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.SliderDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents;
 
 public class _DefaultSliderComponentPartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _DefaultSliderComponentPartial(IHttpClientFactory httpClientFactory)
     {
-        return View();
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7029/api/Sliders");
+
+        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+        var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData);
+        return View(values);
     }
 }
