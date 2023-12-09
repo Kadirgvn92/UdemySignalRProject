3b87f1c0ea7ec14248c6218f024be559d7d9224c
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-09 14:09:53 +0300

    Lesson 105 :I integrated basket entity with abstract classes

---
+++ SignalR.DataAccessLayer/EntityFramework/EfBasketDal.cs
@@ -0,0 +1,24 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.EntityFramework;
+public class EfBasketDal : GenericRepository<Basket>, IBasketDal
+{
+    public EfBasketDal(SignalRContext context) : base(context)
+    {
+    }
+
+    public List<Basket> GetBasketByMenuTableNumber(int id)
+    {
+        using var context = new SignalRContext();
+        var values = context.Baskets.Where(x => x.MenuTableID == id).ToList();
+        return values;
+    }
+}
