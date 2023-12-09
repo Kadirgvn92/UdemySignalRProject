4e7237730ca7b855cdd2c3380d287ed676627dfd
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-09 12:22:38 +0300

    Lesson 100 : Menu Page is prepearing

---
+++ SignalRWebUI/ViewComponents/MenuComponents/_MenuNavbarComponentPartial.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.MenuComponents;
+
+public class _MenuNavbarComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    { return View(); }
+
+}
