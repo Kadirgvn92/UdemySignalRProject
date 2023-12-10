912178d51c1c5e75dbb5117e4a49292a4cda48b2
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-10 13:41:21 +0300

    Lesson 106 : I consumed Basket API

---
+++ SignalRApi/Controllers/BasketsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+
+namespace SignalRApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BasketsController : ControllerBase
+{
+    private readonly IBasketService _basketService;
+
+    public BasketsController(IBasketService basketService)
+    {
+        _basketService = basketService;
+    }
+
+    [HttpGet]
+    public IActionResult GetBasketByMenuTableID(int id)
+    {
+        var values = _basketService.TGetBasketByMenuTableNumber(id);
+        return Ok(values);
+    }
+}
--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -66,6 +66,9 @@ builder.Services.AddScoped<IMenuTableDal, EfMenuTableDal>();
 builder.Services.AddScoped<ISliderService, SliderManager>();
 builder.Services.AddScoped<ISliderDal, EfSliderDal>();
 
+builder.Services.AddScoped<IBasketService, BasketManager>();
+builder.Services.AddScoped<IBasketDal, EfBasketDal>();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
