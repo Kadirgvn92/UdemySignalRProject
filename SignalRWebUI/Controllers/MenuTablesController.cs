d1c263d62ab1165615fcfc2cb8fd088980b42fdc
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-12 21:36:39 +0300

    Lesson 142 : Tables are completed

--- SignalRWebUI/Controllers/MenuTablesController.cs
+++ SignalRWebUI/Controllers/MenuTablesController.cs
@@ -84,4 +84,18 @@ public class MenuTablesController : Controller
 		}
 		return View();
 	}
+
+	[HttpGet]
+	public async Task<IActionResult> TableListByStatus()
+	{
+		var client = _httpClientFactory.CreateClient();
+		var responseMessage = await client.GetAsync("https://localhost:7029/api/MenuTables");
+		if (responseMessage.IsSuccessStatusCode)
+		{
+			var jsonData = await responseMessage.Content.ReadAsStringAsync();
+			var values = JsonConvert.DeserializeObject<List<ResultMenuTableDto>>(jsonData);
+			return View(values);
+		}
+		return View();
+	}
 }
