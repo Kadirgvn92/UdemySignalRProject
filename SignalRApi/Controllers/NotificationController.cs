6064d896447a27f2ef17a2f88d45b1e647876dc7
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-12 10:42:45 +0300

    Lesson 134 : Notification AdminPage API is completed

--- SignalRApi/Controllers/NotificationController.cs
+++ SignalRApi/Controllers/NotificationController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers;
 [Route("api/[controller]")]
@@ -28,5 +30,50 @@ public class NotificationController : ControllerBase
 	{
 		return Ok(_notificationService.TGetAllNotificationByFalse());
 	}
+	[HttpPost]
+	public IActionResult CreateNotification(CreateNotificationDto createNotificationDto)
+	{
+		Notification notification = new Notification()
+		{
+			Description = createNotificationDto.Description,
+			Date = Convert.ToDateTime(DateTime.Now.ToShortDateString()),
+			Icon = createNotificationDto.Icon,
+			Status = false,
+			Type = createNotificationDto.Type
+		};
+		_notificationService.TAdd(notification);
+		return Ok("Ekleme işlemi başarıyla yapıldı");	
+	}
+
+	[HttpDelete]
+	public IActionResult DeleteNotification(int id)
+	{
+		var value = _notificationService.TGetByID(id);
+
+		_notificationService.TDelete(value);
+		return Ok("Bildirim Silindi");
+	}
+	[HttpGet("{id}")]
+	public IActionResult GetNotification(int id)
+	{
+		var value = _notificationService.TGetByID(id);
+		return Ok(value);
+	}
+	[HttpPut]
+	public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+	{
+		Notification notification = new Notification()
+		{
+			NotificationID = updateNotificationDto.NotificationID,
+			Description = updateNotificationDto.Description,
+			Date = updateNotificationDto.Date,
+			Icon = updateNotificationDto.Icon,
+			Status = updateNotificationDto.Status,
+			Type = updateNotificationDto.Type
+		};
+		_notificationService.TUpdate(notification);
+		return Ok("Bildirim Güncellendi.");
+	}
+
 
 }
