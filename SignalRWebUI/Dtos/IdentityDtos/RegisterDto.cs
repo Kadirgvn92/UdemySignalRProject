4fb240ab32d5fd7652acd48a0bcfba3caebce113
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-13 14:17:39 +0300

    Lesson 158 : Register dto is configured

---
+++ SignalRWebUI/Dtos/IdentityDtos/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRWebUI.Dtos.IdentityDtos;
+
+public class RegisterDto
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Username { get; set; }
+    public string Mail { get; set; }
+    public string Password { get; set; }
+}
