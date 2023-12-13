8e902000c0e900611e22f3a2117d70f657ac0269
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-13 13:03:25 +0300

    Lesson 157 : Identity database migration operation is done

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SignalR.EntityLayer.Entities;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SignalR.DataAccessLayer.Concrete;
-public class SignalRContext : DbContext
+public class SignalRContext : IdentityDbContext<AppUser,AppRole,int>
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
---
+++ SignalR.EntityLayer/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.EntityLayer.Entities;
+public class AppUser : IdentityUser<int>
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+}
