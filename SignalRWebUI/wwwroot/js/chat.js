dea1e27776df476d2df6a8eb335d10058ae0cb72
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-13 10:54:58 +0300

    Lesson 150-151 : MessageChat Js is completed

---
+++ SignalRWebUI/wwwroot/js/chat.js
@@ -0,0 +1,33 @@
+﻿
+var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:7029/SignalRHub").build();
+document.getElementById("sendbutton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var currentTime = new Date();
+    var currentHour = currentTime.getHours();
+    var currentMinute = currentTime.getMinutes();
+
+    var li = document.createElement("li");
+    var span = document.createElement("span");
+    span.style.fontWeight = "bold";
+    span.textContent = user
+    li.appendChild(span);
+    li.innerHTML += `:${message} - ${currentHour}:${currentMinute}`;
+    document.getElementById("messagelist").appendChild(li);
+
+});
+
+connection.start().then(function () {
+    document.getElementById("sendbutton").disabled = false;
+}).catch(function (err) {
+    return console.log.error(err.toString())
+});
+
+document.getElementById("sendbutton").addEventListener("click", function (event) {
+    var user = document.getElementById("userinput").value;
+    var message = document.getElementById("messageinput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+       return console.log.error(err.toString());
+    });
+    event.preventDefault();
+});
