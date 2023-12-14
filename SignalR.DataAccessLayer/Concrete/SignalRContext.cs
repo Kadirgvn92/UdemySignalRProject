7262ee65b72967ac69486a18f0b2203317d6a048
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-15 01:53:15 +0300

    Lesson 175 : Message entity updated

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -31,4 +31,5 @@ public class SignalRContext : IdentityDbContext<AppUser,AppRole,int>
     public DbSet<Slider> Sliders { get; set; }
     public DbSet<Basket> Baskets { get; set; }
     public DbSet<Notification> Notifications { get; set; }
+    public DbSet<Message> Messages { get; set; }
 }
