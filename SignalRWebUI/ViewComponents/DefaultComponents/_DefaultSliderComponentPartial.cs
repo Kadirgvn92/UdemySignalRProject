4a6b9940b125eb90a7137e9986fc74fa207064e5
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-15 01:45:23 +0300

    Lesson 173-174 : Some refactoring is made

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -15,7 +15,7 @@ public class _DefaultSliderComponentPartial : ViewComponent
     public async Task<IViewComponentResult> InvokeAsync()
     {
         var client = _httpClientFactory.CreateClient();
-        var responseMessage = await client.GetAsync("https://localhost:7029/api/Sliders");
+        var responseMessage = await client.GetAsync("https://localhost:7029/api/Slider");
 
         var jsonData = await responseMessage.Content.ReadAsStringAsync();
         var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData);
