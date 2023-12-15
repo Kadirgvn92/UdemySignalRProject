b2da7cea05736e4c972778af51d0fd5a6fbceb7f
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-15 14:35:29 +0300

    Lesson 190-192 : Some settings refactored

---
+++ SignalRWebUI/Controllers/SettingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.EntityLayer.Entities;
+using SignalRWebUI.Dtos.IdentityDtos;
+
+namespace SignalRWebUI.Controllers;
+public class SettingController : Controller
+{
+	private readonly UserManager<AppUser> _userManager;
+
+	public SettingController(UserManager<AppUser> userManager)
+	{
+		_userManager = userManager;
+	}
+	[HttpGet]
+	public async Task<IActionResult> Index()
+	{
+		var values = await _userManager.FindByNameAsync(User.Identity.Name);
+		UserEditDto userEditDto = new UserEditDto();
+		userEditDto.Name = values.Name;
+		userEditDto.Surname = values.Surname;
+		userEditDto.Username = values.UserName;
+		userEditDto.Mail = values.Email;
+		return View(userEditDto);
+	}
+}
