d0d688042c2936a85e69eed6eb858b6538aa6b69
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-15 15:02:56 +0300

    Lesson 193 : Setting section updated and use sweetalert

--- SignalRWebUI/Controllers/SettingController.cs
+++ SignalRWebUI/Controllers/SettingController.cs
@@ -23,4 +23,19 @@ public class SettingController : Controller
 		userEditDto.Mail = values.Email;
 		return View(userEditDto);
 	}
+	[HttpPost]
+	public async Task<IActionResult> Index(UserEditDto userEditDto)
+	{
+		if (userEditDto.Password == userEditDto.ConfirmPassword)
+		{
+			var user = await _userManager.FindByNameAsync(User.Identity.Name);
+			user.Name = userEditDto.Name;
+			user.Surname = userEditDto.Surname;
+			user.UserName = userEditDto.Username;
+			user.Email = userEditDto.Mail;
+			user.PasswordHash = _userManager.PasswordHasher.HashPassword(user,userEditDto.Password);
+			await _userManager.UpdateAsync(user);
+		}
+		return  NoContent();
+	}
 }
